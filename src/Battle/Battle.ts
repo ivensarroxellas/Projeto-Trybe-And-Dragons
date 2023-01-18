e6da9dafc078fec379594697250f469281e9a5d2
Ivens <ivensarroxellas@hotmail.com>
2023-01-18 19:24:37 -0300

    Req 10 TOMORROW IS A NEW DAY

--- src/Battle/Battle.ts
+++ src/Battle/Battle.ts
@@ -1,6 +1,6 @@
 import Fighter from '../Fighter';
 
-abstract class Battle {
+class Battle {
   constructor(protected player: Fighter) { }
 
   // Should return 1 if player wins, -1 otherwise
--- src/Battle/PVP.ts
+++ src/Battle/PVP.ts
@@ -1,4 +1,22 @@
+import Character from '../Character';
+import Fighter from '../Fighter';
 import Battle from './Battle';
 
-export default class PVP implements Battle {
+export default class PVP extends Battle {
+  public player: Fighter;
+  public player2: Fighter;
+
+  constructor(player: Fighter, player2: Fighter) {
+    super(player);
+    this.player = player;
+    this.player2 = player2;
+  }
+
+  fight(): number {    
+    this.player.attack(this.player2);
+    if (this.player2.lifePoints < 0) {
+      return this.player.lifePoints === -1 ? -1 : 1;
+    } 
+    return this.player.lifePoints === -1 ? -1 : 1;
+  }
 }
