dbd50a8dbf451cf10700911457574452e4f17d57
Ivens <ivensarroxellas@hotmail.com>
2023-01-18 16:33:20 -0300

    Req 4 DONE

---
+++ src/Archetypes/Warrior.ts
@@ -0,0 +1,19 @@
+import Archetype from './Archetype';
+import { EnergyType } from '../Energy';
+
+export default class Warrior extends Archetype {
+  private static _createdArchetypeInstances = 0;
+  private _energyType: EnergyType;
+  
+  constructor(name : string) {
+    super(name);
+    this._energyType = 'stamina';
+    Warrior._createdArchetypeInstances += 1;
+  }
+
+  get energyType(): EnergyType { return this._energyType; }
+  
+  public static createdArchetypeInstances(): number {
+    return this._createdArchetypeInstances;
+  }
+}
